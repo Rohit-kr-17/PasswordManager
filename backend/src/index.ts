2ad8bcf5805e17a1c3bec4ef7a4a1a620de079ae
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2024-12-04 21:12:56 +0530

    added prod url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:5173"
+  origin: "https://pm17.netlify.app/"
 }))
 
 app.use("/api/user/", userRouter);
