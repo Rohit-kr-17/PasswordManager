93c85690288682c65d67d15da0ed3f585bdb382f
akshmangal005 <akashmangal79@gmail.com>
2025-01-14 20:18:55 +0530

    added onkeydown in inputprops

--- frontend/src/Components/Input.tsx
+++ frontend/src/Components/Input.tsx
@@ -4,6 +4,7 @@ interface InputProps {
   name: string;
   placeholder?: string;
   onChange?: any
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 const Input = (
   { label, type, name, placeholder,...rest }: InputProps,
