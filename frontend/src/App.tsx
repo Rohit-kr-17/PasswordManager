333ff8d50f81cd48684de9ee6f96336254caa944
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2025-03-26 21:29:42 +0530

    fixed logout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 const apiUrl = import.meta.env.VITE_API_URL;
 import { Header } from "./Components/Header";
 import SignIn from "./Pages/SignIn";
